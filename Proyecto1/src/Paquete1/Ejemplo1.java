1e47a7c24e53f93ab82654b5b0cd6468ace2f634
DAW <DAW@DESKTOP-U4FTC0L>
2024-01-16 12:33:25 +0100

    Segunda

--- Proyecto1/src/Paquete1/Ejemplo1.java
+++ Proyecto1/src/Paquete1/Ejemplo1.java
@@ -4,8 +4,7 @@ public class Ejemplo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Ejemplo de control de versiones git");	
-		System.out.println("Segundo ejemplo de versiones git");
+
 	}
 
 }
